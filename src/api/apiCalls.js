2c23fc8faaa26c75e57ba52ab1ba2a1e0f481a2c
popatre <76402245+popatre@users.noreply.github.com>
2023-04-06 15:33:22 +0100

    refac api calls

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 
 const googleBooks = axios.create({
-    baseURL: "https://www.googleapis.com/books/v1",
+    baseURL: "https://www.googleapis.com/books/v1/volumes",
 });
 
 export const getBooks = (query) => {
-    return googleBooks
-        .get(`/volumes`, { params: { q: query } })
-        .then(({ data }) => {
-            return data.items;
-        });
+    return googleBooks.get(`/`, { params: { q: query } }).then(({ data }) => {
+        return data.items;
+    });
 };
 
 export const getPublishersBooks = (publisher, limit = 10) => {
     return googleBooks
-        .get(`/volumes?q=inpublisher:${publisher}&maxResults=${limit}`)
+        .get(`?q=inpublisher:${publisher}&maxResults=${limit}`)
         .then(({ data }) => {
             return data.items;
         });
